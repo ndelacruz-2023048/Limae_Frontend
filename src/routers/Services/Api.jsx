9cb6a391070c34e492e01d45ea111e88f0f87613
ndelacruz-2023048 <ndelacruz-2023048@kinal.edu.gt>
2025-07-13 22:48:46 -0600

    Se coloca el codigo de Api.jsx, ya que hubo un conflicto de nombres de archivo

---
+++ src/routers/Services/Api.jsx
@@ -0,0 +1,42 @@
+import axios from "axios"
+
+const apiClient = axios.create(
+    {
+        baseURL: 'http://localhost:3660/SeminarioProyecto/v1',
+        withCredentials: true,
+        timeout: 10000, 
+    }
+)
+
+export const loginRequest = async (user) => {
+    try {
+        return await apiClient.post('/Auth/login', user);
+    } catch (e) {
+        return {
+            error: true,
+            e
+        }
+    }
+}
+
+export const registerRequest = async (user) => {
+    try {
+        return await apiClient.post('/Auth/register', user);
+    } catch (e) {
+        return {
+            error: true,
+            e
+        }
+    }
+}
+
+export const logoutRequest = async()=> {
+    try {
+        return await apiClient.post('/Auth/logout')
+    } catch (e) {
+        return {
+            error: true,
+            e
+        }
+    }
+}
