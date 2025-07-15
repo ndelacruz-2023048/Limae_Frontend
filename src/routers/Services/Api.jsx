7209dc5b1c4dc87eb9379b836a96d9f86d0234ab
ndelacruz-2023048 <ndelacruz-2023048@kinal.edu.gt>
2025-07-14 23:34:21 -0600

    Update Api.jsx

--- src/routers/Services/Api.jsx
+++ src/routers/Services/Api.jsx
@@ -4,7 +4,7 @@ const API_URL = import.meta.env.VITE_API_URL
 
 const apiClient = axios.create(
     {
-        baseURL: `${API_URL}/SeminarioProyecto/v1`,
+        baseURL: `${API_URL}SeminarioProyecto/v1`,
         withCredentials: true,
         timeout: 10000, 
     }
